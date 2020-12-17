cb151ded259ab7a9f8e58aa0ca032e30f63144d9
Fadila <fadila@storj.io>
2020-12-17 10:48:04 +0100

    copy config files

--- scripts/run.sh
+++ scripts/run.sh
@@ -17,6 +17,6 @@ echo "STORJ_SIM SETUP NETWORK"
 # setup the network
 storj-sim -x  --host 0.0.0.0 --satellites 1 --storage-nodes 10 network --dev --postgres=$STORJ_SIM_POSTGRES setup
 sed -i 's/# metainfo.rate-limiter.enabled: true/metainfo.rate-limiter.enabled: false/g' $(storj-sim network env SATELLITE_0_DIR)/config.yaml
-
+cp -r $(storj-sim network env STORAGENODE_9_DIR) /identity-files
 echo "STORJ_SIM RUN NETWORK"
 storj-sim -x --host 0.0.0.0 --satellites 1 --storage-nodes 9 network --dev run
