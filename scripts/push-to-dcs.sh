7895a90e2e13021430a8d40b5f0b819ad89d6107
Krista Spriggs <krista@storj.io>
2021-04-29 15:28:27 -0400

    Skip UPLINK_CMD fanciness

--- scripts/push-to-dcs.sh
+++ scripts/push-to-dcs.sh
@@ -3,7 +3,6 @@ set -e
 
 [[ -z "${STORJNODE_DEB_ACCESS}" ]] && echo "STORJNODE_DEB_ACCESS envar not set" &&  exit 1
 [[ -z "${STORJNODE_DEB_BUCKET}" ]] && echo "STORJNODE_DEB_BUCKET envar not set" &&  exit 1
-UPLINK_CMD="${UPLINK_CMD}:-/tmp/uplink"
 
 setup_uplink() {
 	curl -LJ0 --output /tmp/uplink.zip https://github.com/storj/storj/releases/download/v1.28.2/uplink_linux_amd64.zip
@@ -16,5 +15,5 @@ paths=$(find apt-repository/www -mindepth 1 -type f)
 
 for p in ${paths[@]}; do
 	echo "-> pushing ${p}"
-	$UPLINK_CMD --access $STORJNODE_DEB_ACCESS cp ${p} sj://$STORJNODE_DEB_BUCKET/${p#apt-repository/www/}
+	/tmp/uplink --access $STORJNODE_DEB_ACCESS cp ${p} sj://$STORJNODE_DEB_BUCKET/${p#apt-repository/www/}
 done
